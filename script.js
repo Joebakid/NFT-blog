5a1946456a37897368ab9f9ef2ea31ba3771825c
joebakid <josephbawo@gmail.com>
2023-06-22 00:44:04 +0100

    firt

---
+++ script.js
@@ -0,0 +1,31 @@
+const icon = document.getElementById("icon");
+
+// Check if the theme preference is stored in localStorage
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+  document.body.classList.add(savedTheme);
+  updateIcon(savedTheme);
+} else {
+  document.body.classList.add("light-theme"); // Default theme
+}
+
+icon.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+
+  // Update the theme preference in localStorage
+  const currentTheme = document.body.classList.contains("dark-theme")
+    ? "dark-theme"
+    : "light-theme";
+  localStorage.setItem("theme", currentTheme);
+  updateIcon(currentTheme);
+});
+
+function updateIcon(theme) {
+  if (theme === "dark-theme") {
+    icon.src = "dark theme icon/sun.png";
+    icon.style.transform = "rotate(180deg)";
+  } else {
+    icon.src = "dark theme icon/moon.png";
+    icon.style.transform = "rotate(0deg)";
+  }
+}
