2f06ac27c22bb23fcc5acd36c88d3fa8b8492ad7
joebakid <josephbawo@gmail.com>
2023-06-24 00:30:31 +0100

    tips and tricks

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const icon = document.getElementById("icon");
-const nftImg = document.getElementById("img-nft-darkside");
+const icon = document.getElementById('icon');
+const nftImg = document.getElementById('img-nft-darkside');
 
 // Check if the theme preference is stored in localStorage
-const savedTheme = localStorage.getItem("theme");
+const savedTheme = localStorage.getItem('theme');
 if (savedTheme) {
   document.body.classList.add(savedTheme);
   updateIcon(savedTheme);
 } else {
-  document.body.classList.add("light-theme"); // Default theme
+  document.body.classList.add('light-theme'); // Default theme
 }
 
-icon.addEventListener("click", function () {
-  document.body.classList.toggle("dark-theme");
+icon.addEventListener('click', function () {
+  document.body.classList.toggle('dark-theme');
 
   // Update the theme preference in localStorage
-  const currentTheme = document.body.classList.contains("dark-theme")
-    ? "dark-theme"
-    : "light-theme";
-  localStorage.setItem("theme", currentTheme);
+  const currentTheme = document.body.classList.contains('dark-theme')
+    ? 'dark-theme'
+    : 'light-theme';
+  localStorage.setItem('theme', currentTheme);
   updateIcon(currentTheme);
 });
 
 function updateIcon(theme) {
-  if (theme === "dark-theme") {
-    icon.src = "dark theme icon/sun.png";
-    icon.style.transform = "rotate(180deg)";
+  if (theme === 'dark-theme') {
+    icon.src = 'dark theme icon/sun.png';
+    icon.style.transform = 'rotate(180deg)';
   } else {
-    icon.src = "dark theme icon/moon.png";
-    icon.style.transform = "rotate(0deg)";
+    icon.src = 'dark theme icon/moon.png';
+    icon.style.transform = 'rotate(0deg)';
   }
 }
